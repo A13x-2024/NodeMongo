590faa59bf9859240c7bb9e26f3511d291d86bf3
Alexander Eklöf <alexander.eklof@iths.se>
2025-01-20 11:47:50 +0100

    added mongoose and MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`listening to ${port}`);
+app.get('/', (req, res) => {
+    res.send('Welcome to my mongoAPI');
+});
+
+app.post ("/blogpost", (req, res) => {
+    res.send('This is a post request');
+});
+
+
+
+mongoose.
+connect("mongodb+srv://Alex:Oakleaf96@alexapi.2jyqy.mongodb.net/Products-API?retryWrites=true&w=majority&appName=AlexAPI")
+.then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+        console.log(`listening to ${port}`);
+      });
 });
